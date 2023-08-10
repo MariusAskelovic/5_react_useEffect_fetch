7d940a85510b436daf9d13583dfce7519b5e1228
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-08-10 19:46:12 +0300

    delete user

--- src/components/users/UsersList.jsx
+++ src/components/users/UsersList.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const usersUrl = 'https://dummyjson.com/users'
+const usersUrl = 'https://dummyjson.com/users?limit=5'
 
 export default function UsersList() {
     const [usersArr, setUsersArr] = useState([])
@@ -13,13 +13,36 @@ export default function UsersList() {
             })
             .catch(console.warn)
     }, [])
-    const [image, firstName, lastName] = usersArr;
 
+    // useEffect(() => {
+    //     // PRADZIA
+    //     fetch(`${usersUrl}/2`, {
+    //         method: 'DELETE',
+    //     })
+    //         .then(res => res.json())
+    //         .then(console.log);
+    //     // PABAIGA
+    // }, [deleteUser])
+
+    function deleteUser(idToDelete) {
+        const newArr = usersArr.filter((userObj) => (
+            userObj.id !== idToDelete
+        ))
+        setUsersArr(newArr);
+    }
+    console.table(usersArr);
     return (
-        <div>
+        <div className="usersBlock">
             {usersArr.map(userObj => (
-                <div key={userObj.id}>
-                    <h3>{userObj.firstName} </h3>
+                <div className="userCard" key={userObj.id}>
+                    <img className="userImg" src={userObj.image} alt={userObj.firstName} />
+                    <div className="userInfoBlock">
+                        <h3 className="userFullName">{userObj.firstName} {userObj.lastName}</h3>
+                        <h4 className="userAge">Age: {userObj.age}</h4>
+                        <h5 className="userEmail">Email: {userObj.email}</h5>
+                        <p>Eyes Color: <span style={{ color: userObj.eyeColor }}>{userObj.eyeColor}</span></p>
+                        <button key={userObj.id} onClick={() => deleteUser(userObj.id)}>Delete</button>
+                    </div>
                 </div>
             ))}
         </div>
